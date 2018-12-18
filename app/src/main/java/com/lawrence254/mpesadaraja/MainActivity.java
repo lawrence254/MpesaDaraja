99315cbc5d8217b19937dc117f0d9ae5bc1be876
Lawrence <lawrenslava1@gmail.com>
2018-12-18 17:47:41 +0300

    Add logic to that will initiate the payments using STK push

--- app/src/main/java/com/lawrence254/mpesadaraja/MainActivity.java
+++ app/src/main/java/com/lawrence254/mpesadaraja/MainActivity.java
@@ -1,13 +1,109 @@
 package com.lawrence254.mpesadaraja;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ApiClient mApiClient;
+    private ProgressDialog mProgressDialog;
+
+    @BindView(R.id.etAmount)
+    EditText mAmount;
+    @BindView(R.id.etPhone)EditText mPhone;
+    @BindView(R.id.btnPay)Button mPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mProgressDialog = new ProgressDialog(this);
+        mApiClient = new ApiClient();
+        mApiClient.setIsDebug(true); //Set True to enable logging, false to disable.
+
+        mPay.setOnClickListener(this);
+
+        getAccessToken();
+
+    }
+
+    public void getAccessToken() {
+        mApiClient.setGetAccessToken(true);
+        mApiClient.mpesaService().getAccessToken().enqueue(new Callback<AccessToken>() {
+            @Override
+            public void onResponse(@NonNull Call<AccessToken> call, @NonNull Response<AccessToken> response) {
+
+                if (response.isSuccessful()) {
+                    mApiClient.setAuthToken(response.body().accessToken);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<AccessToken> call, @NonNull Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view== mPay){
+            String phone_number = mPhone.getText().toString();
+            String amount = mAmount.getText().toString();
+            performSTKPush(phone_number,amount);
+        }
+    }
+
+
+    public void performSTKPush(String phone_number,String amount) {
+        mProgressDialog.setMessage("Processing your request");
+        mProgressDialog.setTitle("Please Wait...");
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.show();
+        String timestamp = Utils.getTimestamp();
+        STKPush stkPush = new STKPush(
+                BUSINESS_SHORT_CODE,
+                Utils.getPassword(BUSINESS_SHORT_CODE, PASSKEY, timestamp),
+                timestamp,
+                TRANSACTION_TYPE,
+                String.valueOf(amount),
+                Utils.sanitizePhoneNumber(phone_number),
+                PARTYB,
+                Utils.sanitizePhoneNumber(phone_number),
+                CALLBACKURL,
+                "test", //The account reference
+                "test"  //The transaction description
+        );
+
+        mApiClient.setGetAccessToken(false);
+
+        mApiClient.mpesaService().sendPush(stkPush).enqueue(new Callback<STKPush>() {
+            @Override
+            public void onResponse(@NonNull Call<STKPush> call, @NonNull Response<STKPush> response) {
+                mProgressDialog.dismiss();
+                try {
+                    if (response.isSuccessful()) {
+                        Timber.d("post submitted to API. %s", response.body());
+                    } else {
+                        Timber.e("Response %s", response.errorBody().string());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<STKPush> call, @NonNull Throwable t) {
+                mProgressDialog.dismiss();
+                Timber.e(t);
+            }
+        });
     }
 }
